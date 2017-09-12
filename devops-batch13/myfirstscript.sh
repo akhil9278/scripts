ce9cdcbe4a8badd679656b8aa9d1652a677c3758
akhil <akhilreddy.venkatessh@gmail.com>
2017-09-12 09:10:30 -0400

    first script

---
+++ devops-batch13/myfirstscript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+##############
+#purpose:To learn shell script
+#owner:akhilreddy.venkatesh@gmail.com
+#version:1.0
+#input:my first line
+#output:none
+###############
+
+echo "hi my name is akhil"
+
